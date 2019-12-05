4e056f36e5c66eb6a0e8f0e32596389c804654fa
Ferdous Jahan <ferdousjahan2@gmail.com>
2019-12-05 16:57:03 +0600

    migration done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Document</title>
+    <title>{{config('app.name', "Don'tKnowYet")}}</title>
 </head>
 <body>
+    @include('includes.navbar')
     @yield('content')
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <h2>web.php -> PagesController -> function index() -> index.blade</h2>
+    <div class="jumbotron text-center">
+        <h1>This is the home page</h1>
+        <h2>{{$title}}</h2>
+        <p>web.php -> PagesController -> function index() -> index.blade</p>
+        <button type="button" href="/login" class="btn btn-outline-primary">Login</button>
+        <button type="button" href="/register" class="btn btn-outline-success">Register</button>
+    </div>
 @endsection
