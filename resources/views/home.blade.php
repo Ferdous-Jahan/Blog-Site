18f0578146834736979dfedc52a362856d4ab887
Ferdous Jahan <ferdousjahan2@gmail.com>
2019-12-21 18:58:13 +0600

    relationship done between models

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    @if (count($posts) > 0)
+        @foreach ($posts as $post)
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h3><a href="{{route('show', $post->id)}}" >{{$post->title}}</a></h3>
+                    <small>Created on {{$post->created_at}}</small>
+                    <br><br>
+                    <a type="button" href="{{route('edit', $post->id)}}" class="btn btn-outline-primary">Edit</a>
+                    <a type="button" href="{{route('delete', $post->id)}}" onclick="return confirm('Are you sure?')" class="btn btn-outline-danger">Delete</a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+            </div>          
+        @endforeach
+        {{-- {{ $posts->links() }} --}}
+    @else
+        <p>No Post Found</p>
+    @endif
 @endsection
+
