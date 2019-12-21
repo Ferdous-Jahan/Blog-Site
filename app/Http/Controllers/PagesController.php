001bcf6c136c44c053d7d0539000d8fb714846ba
Ferdous Jahan <ferdousjahan2@gmail.com>
2019-12-21 20:48:44 +0600

    access control updated

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,17 +10,4 @@ class PagesController extends Controller
         $title = "data passing from controller with compact(____)";
         return view('pages.index', compact('title'));
     }
-
-    public function about(){
-        $title = "data passing from controller with ->with('___',___)";
-        return view('pages.about')->with('title', $title);
-    }
-
-    public function services(){
-        $data = array(
-            'title' => 'our services',
-            'services' => ['image manage', 'gallery', 'account']
-        );
-        return view('pages.services')->with($data);
-    }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,7 +9,7 @@
             <input type="text" class="form-control" id="title" name='title' placeholder="put your blog title" required>
             <hr>
             <label for="comment"><h3>Body</h3></label>
-            <textarea class="form-control" rows="10" id="body" name='body' placeholder="post details" required></textarea>
+            <textarea class="form-control" rows="10" id="body" name='body' style="resize : none;" placeholder="post details" required></textarea>
             <hr>
             <a type="button" href="/posts" class="btn btn-outline-primary">Back</a>
             <input type="submit" name="submit" class="btn btn-outline-success" value="Submit" style="float: right;">
